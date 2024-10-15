35ee540f72ecaaf643e302cd408612815e4f4997
vajaleni <lena37376@gmail.com>
2024-10-15 13:36:18 +0300

    change cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    chromeWebSecurity: true,
+   experimentalSessionAndOrigin: true,
+   defaultCommandTimeout: 10000,
+   viewportWidth: 1280,
+   viewportHeight: 720,
   },
 });
