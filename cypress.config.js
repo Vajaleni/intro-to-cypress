6dae8afb25025ecb0c90fcff55feed537d0ab853
vajaleni <lena37376@gmail.com>
2024-10-17 15:49:16 +0300

    Add homework18

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   e2e: {
     chromeWebSecurity: true,
    experimentalSessionAndOrigin: true,
-   defaultCommandTimeout: 10000,
+   defaultCommandTimeout: 60000,
    viewportWidth: 1280,
    viewportHeight: 720,
   },
